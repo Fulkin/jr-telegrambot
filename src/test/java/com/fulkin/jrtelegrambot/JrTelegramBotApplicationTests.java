da947cd385ddf25025c6342616d41eb56823ee86
Fulkin <sea-xv@mail.ru>
2021-11-18 13:42:52 +0500

    JRBT-0: added SpringBoot skeleton to the project.

---
+++ src/test/java/com/fulkin/jrtelegrambot/JrTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fulkin.jrtelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
